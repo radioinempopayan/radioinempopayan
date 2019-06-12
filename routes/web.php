126ddf693d3db89dc87fbf1f7cbb1fd7d3ad7ff0
magostudy <jacuetia8@misena.edu.co>
2019-06-12 13:55:45 -0500

    Revert "implementacion de funcionalidades  laravel"
    This reverts commit 92468eee3d952269bd58a162ef98e5387cfb344b.

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('index');
-
-Route::get('galeria', function () {
-    return view('galeria');
-})->name('galery');
-
-Route::get('about', function () {
-    return view('about');
-})->name('abouts');
-
-Route::get('prueba', function () {
-	
-	$trabajo = ['malo','bueno','odio','risa','gracias','varios','open','templatess','atom','sublime','carai','dicho','halo','infinity','work','117'];
-
-    //return view('beta',['trabajos'=>$trabajo]);
-    return view ('beta',compact('trabajo'));
-})->name('test');
+    return view('welcome');
+});
